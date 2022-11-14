5a22e3c1a191883addd09ace02331f661d1d729c
sakthimuthuselvan <sakthimsd531@gmail.com>
2022-11-14 18:42:54 +0530

    my portfolio

--- logo/main.js
+++ logo/main.js
@@ -1,70 +1,97 @@
-let color=["#D7DBDD ","#F9E79F","#AEB6BF","#AED6F1","#5D6D7E ","#53B3F4","#F3BE53","#95A5A6","#F0B27A","#76D7C4","#76D7C4"];
-let b=0;
+// let color=["#D7DBDD ","#F9E79F","#AEB6BF","#AED6F1","#5D6D7E ","#53B3F4","#F3BE53","#95A5A6","#F0B27A","#76D7C4","#76D7C4"];
+// let b=0;
 
 setInterval(()=>{
-    if(b==12){
-        b=0;
-    }
+    // if(b==12){
+    //     b=0;
+    // }
    
-    let logo=document.getElementById("logos");
-    let chg=color[b];
-    logo.style.backgroundColor=chg;
-    b++;
-},2000);
+    // let logo=document.getElementById("logos");
+    // let chg=color[b];
+    // logo.style.backgroundColor=chg;
+    // b++;
+    let randam=Math.floor(Math.random()* 1234)
+    // let randam=Math.floor(Math.randam()*13234)
+    let logo=document.getElementById("logos")
+    logo.style.backgroundColor="#"+randam
 
-
-// let what=false
-//   function radio(){
-    
-//     let  btn1=document.getElementById("btn1")
-//     let a=document.getElementById("radio1").value
-//     what=!what
-//     if((a==1)&&(what==false)){
-//         btn1.style.display="block"
-
-//     }else{
-//         btn1.style.display="none"
-//     }
-//   }
-//   radio()
+},1000);
 
 var easy=false;
+var easy1=false;
 
 function radio(){
     easy=!easy;
-   let radio=document.getElementById("radio1").value
+
+   let sakthi=document.getElementById("radio1").value
    let btn=document.getElementById("btn1")
-   if((radio==1)&&(easy==true)){
+   let btn2=document.getElementById("btn2")
+   if(easy==true){
         btn.style.display="block";
+        btn2.style.display="none"
+        easy=false;
+     
     }else{
         btn.style.display="none";
+        btn2.style.display="block"
+       
     }
 }
 
 function radio1(){
-    easy=!easy;
-    let radio=document.getElementById("radio1").value
+    
+    easy1=!easy1;
+  
+    let simeon=document.getElementById("radio1").value
     let btn=document.getElementById("btn2")
-    if((radio==1)&&(easy==true)){
+    let btn1=document.getElementById("btn1")
+    if(easy1==true){
          btn.style.display="block";
+         btn1.style.display="none";
+         easy1=false;
      }else{
         btn.style.display="none";
+        btn1.style.display="block";
      }
  }
 
-let arr=["developer","fresher"]
-let count=0;
-let index=0;
-let counttext=""
-let indextext=""
- function sakthi(){
-let span=document.getElementById("span");
-
-counttext=arr[count]
-indextext=counttext.slice(0,index++)
-    console.log(indextext)
-
-    span.innerText=indextext
-    setTimeout(sakthi,100)
+let click=false
+let nav=document.getElementById("navsec")
+function menu(){
+    click=!click
+    console.log(click)
+    if(click==true){
+        nav.style.display="block" 
+    
+    }else{
+        nav.style.display="none" 
+    }
 }
-sakthi()
+
+
+
+const blink=["developer","fresher"]
+let index=0
+let count=0
+
+let textcontent=""
+var letter=""
+
+
+function no(){
+    if(index===blink.length){
+        index=0
+    }
+    textcontent=blink[index]
+    letter=textcontent.slice(0,++count)
+
+    document.getElementById("span").innerHTML=letter
+    if(letter.length===textcontent.length){
+        index++;
+        count=0
+        
+    }
+    setTimeout(no,500)
+
+}
+no()
